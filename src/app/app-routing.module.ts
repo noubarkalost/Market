10f67cffeb5f288eba7aca31020fb3fd11154b25
Kalost, Noubar <noubar.kalost1@wolterskluwer.com>
2022-08-21 08:10:14 +0400

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
